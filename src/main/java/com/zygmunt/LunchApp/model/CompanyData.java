e47d261c2f85115d8d65d48140312465d25798f5
zygmunt7 <zygmunt.jasinski@pollub.edu.pl>
2023-01-12 22:22:09 +0100

    Class CompanyData

--- src/main/java/com/zygmunt/LunchApp/model/CompanyData.java
+++ src/main/java/com/zygmunt/LunchApp/model/CompanyData.java
@@ -1,4 +1,79 @@
 package com.zygmunt.LunchApp.model;
 
+import jakarta.validation.constraints.NotNull;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.persistence.Embedded;
+
+@Embeddable
 public class CompanyData {
+
+    @NotNull
+    @Column(name = "companyName")
+    private String name;
+
+    @Embedded
+    @NotNull
+    private Address address;
+
+    @NotNull
+    private String NIP;
+
+    @NotNull
+    private String REGON;
+
+    @NotNull
+    private String phone;
+
+    @NotNull
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public String getNIP() {
+        return NIP;
+    }
+
+    public void setNIP(String NIP) {
+        this.NIP = NIP;
+    }
+
+    public String getREGON() {
+        return REGON;
+    }
+
+    public void setREGON(String REGON) {
+        this.REGON = REGON;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
