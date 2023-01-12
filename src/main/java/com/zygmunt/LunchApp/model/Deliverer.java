06e6064216327c48b25ea0f64ec8db13549a97b7
zygmunt7 <zygmunt.jasinski@pollub.edu.pl>
2023-01-12 22:41:00 +0100

    Deliverer

--- src/main/java/com/zygmunt/LunchApp/model/Deliverer.java
+++ src/main/java/com/zygmunt/LunchApp/model/Deliverer.java
@@ -1,4 +1,31 @@
 package com.zygmunt.LunchApp.model;
 
-public class Deliverer {
+
+import io.micrometer.common.lang.Nullable;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Null;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import java.util.List;
+
+@Entity
+@DiscriminatorValue("deliverer")
+public class Deliverer extends Employee{
+
+    @Nullable
+    @OneToMany(mappedBy = "deliverer")
+    private List<Order> orders;
+
+    @Nullable
+
+    @Nullable
+    public List<Order> getOrders() {
+        return orders;
+    }
+
+    public void setOrders(@Nullable List<Order> orders) {
+        this.orders = orders;
+    }
 }
